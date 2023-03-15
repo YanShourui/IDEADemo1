393af0e4576a8cc02b9f407c0b51a43e8848772f
Yan <1478893835@qq.com>
2023-03-15 09:13:37 +0800

    first commit

---
+++ src/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * ClassName: ${NAME}
+ * Package:
+ * Description:
+ *
+ * @Author: 闫守瑞
+ * @Create: ${DATE} - ${TIME}
+ * @Version: v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
