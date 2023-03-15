a7f2fc5c96a2ef1334e9222cfa671225a8d9620c
Yan <1478893835@qq.com>
2023-03-15 09:14:33 +0800

    second commit

--- src/test/Main.java
+++ src/test/Main.java
@@ -11,6 +11,6 @@ package test;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!01");
     }
 }
